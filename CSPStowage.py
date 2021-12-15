88cd4222f88f08b3cafddb3bfc669d115c0de319
jesus <100429124@alumnos.uc3m.es>
2021-12-15 21:15:10 +0100

    Parte 1

--- CSPStowage.py
+++ CSPStowage.py
@@ -36,25 +36,38 @@ def notEqual(*args):
 def under(*args):
     for i in range(len(args)):
         for j in range(len(args)):
-            if (i != j) and (args[i][0] != args[j][0] or args[i][1] != args[j][1]-1) and (mapa[args[i][0]][args[i][1]+1] != "X"):
-                return False
-    return True
+            if i != j:
+                if mapa[args[i][1] + 1][args[i][0]] == "X":
+                    valores.append(True)
+                else:
+                    valores.append(args[i][0] == args[j][0] and args[i][1] == args[j][1] - 1)
+        valoresJefe.append(any(valores))
+    return all(valoresJefe)
 
 
-def domain(contenedor, mapa):
-    domain = []
-    if contenedor[2] == "S":
-        for index_c, fila in enumerate(mapa):
-            for index_f, elem in enumerate(fila):
-                if elem == "N" or elem == "E":
-                    domain.append([index_c, index_f])
-    elif contenedor[2] == "R":
-        for index_c, fila in enumerate(mapa):
-            for index_f, elem in enumerate(fila):
-                if elem == "E":
-                    domain.append([index_c, index_f])
-    return domain
+def notOn(*args):
+    valoresJefe = []
+    for i in range(len(args)):
+        valores = []
+        for j in range(len(args)):
+            if i != j:
+                if puertos[str(int(i) + 1)] == 1 and puertos[str(int(j) + 1)] == 2:
+                    if args[i][0] == args[j][0]:
+                        valores.append(args[i][1] != args[j][1] + 1)
+                    else:
+                        valores.append(True)
+        valoresJefe.append(all(valores))
+    return all(valoresJefe)
 
 
-print(problem(mapa, contenedores)[0])
-# print(domain(contenedores[4], mapa))
+def printSol(sol):
+    columnas = len(mapa[0]) - 1
+    filas = len(mapa)
+    mat = [[" " for _ in range(columnas)] for _ in range(filas)]
+    for key in sol.keys():
+        mat[sol[key][1]][sol[key][0]] = key
+    for fila in mat:
+        print(fila)
+
+
+printSol(problem(mapa, contenedores)[0])
